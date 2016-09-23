24e0ebbb53ecfc240e9b0ecbd78a292f7e07e4ac
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-09-23 15:09:17 -0400

    add make_private method

--- lib/github.rb
+++ lib/github.rb
@@ -16,4 +16,8 @@ class GitHub
   def get_commit(repo, commit_id)
     @client.commit(repo, commit_id)
   end
+
+  def make_private(repo)
+    @client.edit_repository(repo, private: true)
+  end
 end
