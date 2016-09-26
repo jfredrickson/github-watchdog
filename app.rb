1ba3600fd5c09379dfcd421b88fd1a3552f7e096
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-09-26 17:43:54 -0400

    do all the things

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require_relative "config/environment"
 
 class WatchdogApp < Sinatra::Base
   post "/push" do
+    logger.info "Received push webhook"
+
     # Check for valid JSON
     begin
       payload = JSON.parse(request.body.read)
     rescue JSON::ParserError
+      logger.warn "Invalid JSON payload"
       status 400
       return
     end
@@ -13,9 +16,24 @@ class WatchdogApp < Sinatra::Base
     # Check for valid data
     begin
       head_commit_id = payload.fetch("head_commit").fetch("id")
+      repo = payload.fetch("repository").fetch("full_name")
     rescue KeyError
+      logger.warn "Invalid webhook payload"
       status 422
       return
     end
+
+    logger.info "Analyzing commit #{head_commit_id} on repository #{repo}"
+    github = GitHub.new(settings.github_token)
+    commit = github.get_commit(repo, head_commit_id)
+    analyzer = Analyzer.new(commit)
+    patterns = settings.patterns
+    logger.info "Patterns: #{patterns.count}"
+    matches = analyzer.match(patterns)
+    unless matches.empty?
+      logger.info "Matches found: #{matches.count}"
+      github.make_private(repo)
+    end
+    return
   end
 end
